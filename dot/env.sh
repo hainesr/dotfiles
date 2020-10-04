127423b873b77081226b9cd6313d33dd42477e98
Robert Haines <rhaines@manchester.ac.uk>
2020-10-04 21:11:36 +0100

    Add `EDITOR` environment, set to `vi`.

--- dot/env.sh
+++ dot/env.sh
@@ -4,6 +4,14 @@
 # Robert Haines.
 #
 
+# Default editor
+
+# Set both of these to `vi` as there is rarely a need for a line editor these
+# days. I'm not using `vim` here in case I haven't installed it yet when I need
+# an editor! If `vim` is installed then calling `vi` runs `vim` anyway.
+export EDITOR=vi
+export VISUAL=vi
+
 # Python
 
 # From https://docs.python-guide.org/dev/pip-virtualenv/#requiring-an-active-virtual-environment-for-pip
